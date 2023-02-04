a8815255aec1a1e1f366216a15fb4edc55b6f178
kontb2a22 <ahmedxd.2009@gmail.com>
2023-02-04 07:21:23 +0200

    yes
    yes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,12 @@
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <footer>
+      <span className="love">
+        Made with <i class="fa fa-heart pulse"></i> in{" "}
+        <a href="#" target="_blank">
+          Cairo 🇪🇬
+        </a>
+      </span>
+    </footer>
+  );
 }
