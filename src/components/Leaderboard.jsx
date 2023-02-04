9000e874f19f383e62344d3907d3efd446c1f4c3
kontb2a22 <ahmedxd.2009@gmail.com>
2023-02-04 06:25:58 +0200

    update
    hey

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,25 +1,25 @@
 import { useState } from "react";
 import lbIcon from "../leaderboardIcon.svg";
 export default function Leaderboard({ leaderboard, userId }) {
+  let compareFunction;
   const [sortBy, setSortBy] = useState("rolls"); // default is to sort by rolls count
   switch (sortBy) {
     case "rolls":
-      leaderboard = leaderboard.sort((a, b) => a.count - b.count);
+      compareFunction = (a, b) => a.count - b.count;
       break;
     case "time":
-      leaderboard = leaderboard.sort((a, b) => a.timeTaken - b.timeTaken);
+      compareFunction = (a, b) => a.timeTaken - b.timeTaken;
       break;
     case "both":
-      leaderboard = leaderboard.sort(
-        (a, b) => a.count + a.timeTaken - (b.count + b.timeTaken)
-      );
+      compareFunction = (a, b) =>
+        a.count + a.timeTaken - (b.count + b.timeTaken);
       break;
   }
-  leaderboard = leaderboard.slice(0, 10); // top 10
+  leaderboard = leaderboard.sort(compareFunction).slice(0, 10); // top 10
 
   let elesArr = leaderboard.map(({ name, timeTaken, count, id }) => {
     return (
-      <li className={`${userId === id && "choosen"}`}>
+      <li className={`${userId === id ? "choosen" : ""}`}>
         <span>{name}</span>
         <span>{count}</span>
         <span>{timeTaken}</span>
@@ -33,9 +33,16 @@ export default function Leaderboard({ leaderboard, userId }) {
         <span>sort by</span>
         <button onClick={() => setSortBy("rolls")}>rolls</button>
         <button onClick={() => setSortBy("time")}>time</button>
-        <button onClick={() => setSortBy("both")}>both!</button>
+        <button onClick={() => setSortBy("both")}>best of both!</button>
       </div>
-      <ol className="leaderboard-list">{elesArr}</ol>
+      <ol className="grid">
+        <li class="false">
+          <span>Name</span>
+          <span>Rolls</span>
+          <span>Time</span>
+        </li>
+        {elesArr}
+      </ol>
     </div>
   );
 }
