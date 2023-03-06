e6be088c9f753065914585e89aa2989e994772d4
kontb2a22 <ahmedxd.2009@gmail.com>
2023-03-06 06:12:57 +0200

    updates minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,141 @@
-import { useState, useEffect } from "react";
-import Die from "./components/Die";
+import { useEffect, useReducer } from "react";
+import axios from "axios";
 import congratsMp3 from "./congrats.mp3";
+import Die from "./components/Die";
 import Confetti from "react-confetti";
 import Leaderboard from "./components/Leaderboard";
 import Suggestions from "./components/Suggestions";
 import Header from "./components/Header";
-import { nanoid } from "nanoid";
-import axios from "axios";
 import TimeElapsedDisp from "./components/TimeElapsedDisp";
+
 function App() {
+  const congratsAudio = new Audio(congratsMp3);
   const pantryID = "319f2108-7202-4669-9979-bfbd309ebdd7";
   const pantryBasketName = "Leaderboards";
-  const congratsAudio = new Audio(congratsMp3);
 
-  const [dice, setDice] = useState(() => allNewDice());
-  const [tenzies, setTenzies] = useState(false);
-  const [heldDice, setheldDice] = useState([...Array(10)]);
-  // const [leaderboard, setLeaderboard] = useState(
-  //   () => JSON.parse(localStorage.getItem("leaderboard")) || []
-  // );
-  const [leaderboard, setLeaderboard] = useState([]);
-  const [userName, setuserName] = useState(
-    () => localStorage.getItem("tenziesName") || "Guest"
+  function tenziesReducer(state, action) {
+    switch (action.type) {
+      case "start_game": {
+        return {
+          ...state,
+          startTime: new Date().getTime(),
+          interval: action.payload.interval,
+        };
+      }
+      case "win_game": {
+        return {
+          ...state,
+          tenzies: true,
+          interval: null,
+        };
+      }
+      case "play_again": {
+        return {
+          ...state,
+          dice: allNewDice(),
+          count: 0,
+          startTime: 0,
+          interval: null,
+          tenzies: false,
+        };
+      }
+      case "inc_count": {
+        return {
+          ...state,
+          count: state.count + 1,
+        };
+      }
+      case "roll": {
+        return {
+          ...state,
+          dice: state.dice.map((e) =>
+            !e.isHeld ? { ...e, value: ~~(Math.random() * 6) + 1 } : e
+          ),
+        };
+      }
+      case "update_state": {
+        return {
+          ...state,
+          [action.payload.propName]: action.payload.value,
+        };
+      }
+
+      case "fix_die": {
+        // don't hold the dice unless the user started the game and don't make him be able to select the dies after he wins
+        // else it will trigger the useEffect's callback function and will add his name to the leaderboards again!
+        let arr = [...state.dice];
+        arr[action.payload.index].isHeld = !arr[action.payload.index].isHeld;
+        return {
+          ...state,
+          dice: arr,
+        };
+      }
+      default: {
+        return state;
+      }
+    }
+  }
+  function initializeState(state) {
+    return {
+      ...state,
+      dice: allNewDice(),
+      userName: localStorage.getItem("tenziesName") || "Guest",
+    };
+  }
+  const [state, dispatch] = useReducer(
+    tenziesReducer,
+    {
+      dice: [],
+      tenzies: false,
+      userName: "",
+      userId: "",
+      count: 0,
+      startTime: 0,
+      interval: null,
+      leaderboard: [],
+    },
+    initializeState
   );
-  const [userId, setUserId] = useState(0);
-  const [count, setCount] = useState(0);
-  const [startTime, setStartTime] = useState(0);
-  const [interval, setInterval] = useState(null);
 
-  // this is what made possisetIntervalble to stop the interval
+  function allNewDice() {
+    return [...Array(10)].map(() => ({
+      value: ~~(Math.random() * 6) + 1,
+      isHeld: false,
+    }));
+  }
+  function checkWinCond(dice) {
+    return dice.every(
+      (die, i, arr) => die.isHeld && die.value === arr[0].value
+    ); // checking if each die is held and if they are all equal to the first die's value which is a valid approach to check if they are all the same
+  }
+  function rollDice() {
+    if (!tenzies) {
+      dispatch({ type: "inc_count" });
+      if (count === 0) {
+        dispatch({ type: "start_game", payload: { interval: 20 } });
+        return;
+      }
+      dispatch({ type: "roll" });
+    } else {
+      dispatch({ type: "play_again" });
+    }
+  }
 
+  let {
+    count,
+    startTime,
+    interval,
+    userName,
+    tenzies,
+    dice,
+    userId,
+    leaderboard,
+  } = state;
+  console.log(state);
   // USEEFFECT to load leaderboard from pantry to leaderboard state
   useEffect(() => {
     const myHeaders = new Headers();
+
     let arr = [];
     myHeaders.append("Content-Type", "application/json");
 
@@ -46,111 +149,63 @@ function App() {
 
     axios(config).then((result) => {
       result = result.data;
-      Object.keys(result).forEach((e) => {
-        arr.push({ id: e, ...result[e] });
+      Object.keys(result).forEach((e) => arr.push({ id: e, ...result[e] }));
+      dispatch({
+        type: "update_state",
+        payload: { propName: "leaderboard", value: arr },
       });
-      setLeaderboard(arr);
     });
   }, []);
 
   useEffect(() => {
-    if (compareArrays(heldDice, dice)) {
-      setTenzies(true);
-      setInterval(null);
+    if (checkWinCond(dice)) {
+      dispatch({ type: "win_game" });
       congratsAudio.play();
-
       // push num of rolls and time taken to leaderboard
       let endTime = new Date();
       let timeDiff = ((endTime - startTime) / 1000).toFixed(2);
 
-      const obj = { name: userName, count: count, timeTaken: timeDiff };
-      obj.id = nanoid();
-      setUserId(obj.id);
-      // update our leaderboard JSON with the new record
-
-      const data = JSON.stringify({
-        [obj.id]: obj,
+      const obj = {
+        id: crypto.randomUUID(),
+        name: userName,
+        count: count,
+        timeTaken: timeDiff,
+      };
+      dispatch({
+        type: "update_state",
+        payload: { propName: "id", value: obj.id },
       });
 
+      // update our leaderboard JSON with the new record
       const config = {
         method: "put",
         url: `https://getpantry.cloud/apiv1/pantry/${pantryID}/basket/${pantryBasketName}`,
         headers: {
           "Content-Type": "application/json",
         },
-        data: data,
+        data: JSON.stringify({
+          [obj.id]: obj,
+        }),
       };
-
-      axios(config).then(function (response) {
-        //console.log(JSON.stringify(response.data));
+      axios(config).then((response) => {
+        /*console.log(JSON.stringify(response.data)); */
       });
-
-      setLeaderboard((prevLeaderBoard) => {
-        return [...prevLeaderBoard, obj];
+      dispatch({
+        type: "update_state",
+        payload: { propName: "leaderboard", value: [...leaderboard, obj] },
       });
-    } else {
-      setTenzies(false);
     }
-  }, [heldDice]);
-  // the reason why we have heldDice as a dependency instead of dice array is because the last move would be holding the last dice not rolling the last dice
+  }, [dice]);
 
-  // useEffect(() => {
-  //   // localStorage.setItem("leaderboard", JSON.stringify(leaderboard));
-  // }, [leaderboard]);
-
-  function compareArrays(a, b) {
-    if (JSON.stringify(a) === JSON.stringify(b)) {
-      // if the two arrays are equal (doesn't mean a winning condition yet)
-      // further check if there's only one unique element is selected on both (hence the new Set()) and equal on both
-      let reducedA = new Set(a);
-      let reducedB = new Set(a);
-      let cond1 = [...reducedA][0] === [...reducedB][0];
-      let cond2 = reducedA.size === 1 && reducedB.size === 1; // best practice
-      //let cond2 = (reducedA.size === reducedB.size) == 1; // shorter and should work as reducedA.size === reducedB.size would work
-      return cond1 && cond2; // returns true or false based on the conditions
-    } else {
-      return false;
-    }
-  }
-
-  function allNewDice() {
-    return [...Array(10)].map((_) => ~~(Math.random() * 6) + 1);
-  }
-
-  function rollDice() {
-    if (!tenzies) {
-      setCount((prevCount) => prevCount + 1);
-      if (count === 0) {
-        // start the counter but don't roll the dices that have been initialized the user just started !
-        setStartTime(new Date().getTime());
-        setInterval(10);
-        return;
-      }
-      setDice((prevDiceSet) => {
-        for (let i = 0; i < prevDiceSet.length; i++) {
-          if (heldDice[i] !== undefined) continue; // skips the dies that are held
-          prevDiceSet[i] = ~~(Math.random() * 6) + 1;
-        }
-        return prevDiceSet;
-      });
-    } else {
-      // user decides to play again
-      setheldDice([...Array(10)]);
-      setDice(allNewDice());
-      setCount(0);
-      setStartTime(0);
-      setInterval(null);
-    }
-  }
   let diceArr = dice.map((e, i) => (
     <Die
       key={i}
       index={i}
-      value={e}
       count={count}
-      setheldDice={setheldDice}
-      heldDice={heldDice}
       tenzies={tenzies}
+      value={e.value}
+      isHeld={e.isHeld}
+      dispatch={dispatch}
     />
   ));
 
@@ -171,12 +226,12 @@ function App() {
           <div className="dice-container-bottom">
             <span className="roll-number">{count}</span>
             <button className="roll-dice" onClick={rollDice}>
-              {!tenzies ? `${count === 0 ? `Start!` : `Roll`}` : `Play again!`}
+              {!tenzies ? `${count ? "Roll" : "Start"}` : "Play again!"}
             </button>
             <TimeElapsedDisp interval={interval} startTime={startTime} />
           </div>
         </div>
-        <Suggestions userName={userName} setUserName={setuserName} />
+        <Suggestions dispatch={dispatch} userName={userName} />
       </main>
     </>
   );
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,25 +1,20 @@
 export default function Die({
-  setheldDice,
   index,
   value,
-  heldDice,
+  isHeld,
   count,
   tenzies,
+  dispatch,
 }) {
   const styles = {
-    backgroundColor: heldDice[index] != undefined ? "#59e391" : "#fff",
+    backgroundColor: isHeld ? "#59e391" : "#fff",
   };
-
-  function fixDice(e, index) {
-    if (count === 0 || tenzies) return; // don't hold the dice unless the user started the game and don't make him be able to select the dies after he wins
-    // else it will trigger the useEffect's function and will his name to the leaderboards again!
-    setheldDice(function (prevState) {
-      prevState[index] = prevState[index] !== undefined ? undefined : value;
-      return [...prevState];
-    });
+  function fixDie() {
+    if (count === 0 || tenzies) return;
+    dispatch({ type: "fix_die", payload: { index } });
   }
   return (
-    <div className="dice" style={styles} onClick={(e) => fixDice(e, index)}>
+    <div className="dice" style={styles} onClick={fixDie}>
       {value}
     </div>
   );
